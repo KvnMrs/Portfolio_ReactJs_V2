84531e82dd3aa8dcc4d28cf05cb789fb1955a5ed
Kévin Marais <kevin.mrs2020@gmail.com>
2022-07-30 18:59:37 +0200

    Creating About component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 // Components
 import BarNav from "./components/navigation/BarNav";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
   return (
     <>
       <BarNav />
       <Home />
+      <About />
     </>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <section className="About-container">
+        <article>
+          <aside>
+            <h2>En quelques mots...</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+              auctor lacus nec sem volutpat tristique. Interdum et malesuada
+              fames ac ante ipsum primis in faucibus. Aenean rutrum eros vitae
+              metus mollis bibendum. Aenean ultricies euismod turpis, et maximus
+              ex. Mauris blandit, est accumsan molestie rutrum, nisl urna
+              maximus ligula, nec condimentum lectus mauris eu leo. Morbi
+              euismod, magna id auctor volutpat, sem tellus pulvinar eros, vel
+              accumsan arcu felis et nibh. Nulla convallis massa in lacus
+              lacinia, in placerat elit mattis. Suspendisse et lacinia leo.
+            </p>
+          </aside>
+          <div>
+            <img />
+          </div>
+        </article>
+        <article>
+          <div>
+            <img />
+          </div>
+          <aside>
+            <h2>En quelques mots...</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+              auctor lacus nec sem volutpat tristique. Interdum et malesuada
+              fames ac ante ipsum primis in faucibus. Aenean rutrum eros vitae
+              metus mollis bibendum. Aenean ultricies euismod turpis, et maximus
+              ex. Mauris blandit, est accumsan molestie rutrum, nisl urna
+              maximus ligula, nec condimentum lectus mauris eu leo. Morbi
+              euismod, magna id auctor volutpat, sem tellus pulvinar eros, vel
+              accumsan arcu felis et nibh. Nulla convallis massa in lacus
+              lacinia, in placerat elit mattis. Suspendisse et lacinia leo.
+            </p>
+          </aside>
+        </article>
+      </section>
+    </>
+  );
+}
+
+export default About;
