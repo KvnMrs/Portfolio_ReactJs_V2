78dea0d9fbd7820031c9f8889b63a78dde491286
Kévin Marais <kevin.mrs2020@gmail.com>
2022-07-30 17:18:04 +0200

    Creating Home component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 
 // Components
 import BarNav from "./components/navigation/BarNav";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <>
       <BarNav />
+      <Home />
     </>
   );
 }
