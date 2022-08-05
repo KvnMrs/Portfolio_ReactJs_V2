d0423c5a33f0dbd4fe7131637b042c0ea5afe4a8
Kévin Marais <kevin.mrs2020@gmail.com>
2022-08-05 10:49:13 +0200

    Creating Projects component

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import DatasProjects from "../../datas/DatasProjects";
+import ProjectCard from "../../sub-components/projects/ProjectCard";
+
+function Projects() {
+  return (
+    <>
+      <section id="projects" className="Projects-container">
+        <h2>Projets</h2>
+        <div>
+          {DatasProjects.map((el) => (
+            <>
+              <ProjectCard
+                img={el.img}
+                title={el.title}
+                category={el.category}
+                description={el.description}
+                link={el.link}
+              />
+            </>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Projects;
