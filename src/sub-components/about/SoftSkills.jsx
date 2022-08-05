fcce9d057cb51eef116dd4332724b2492a802b84
Kévin Marais <kevin.mrs2020@gmail.com>
2022-08-05 16:57:09 +0200

    Creating SoftSkills sub-component

---
+++ src/sub-components/about/SoftSkills.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import dataSkills from "../../datas/DatasSkills";
+
+const SoftSkills = () => {
+  return (
+    <>
+      {dataSkills
+        .filter((atout) => atout.catégorie === "Atout")
+        .map((el) => (
+          <>
+            <aside>
+              <h4>{el.element}</h4>
+              <div>
+                <img alt={el.catégorie} src={el.image} />
+              </div>
+            </aside>
+          </>
+        ))}
+    </>
+  );
+};
+
+export default SoftSkills;
