b72c21aa5e502a8c323be5603d7465084911e21e
Kévin Marais <kevin.mrs2020@gmail.com>
2022-08-11 23:10:48 +0200

    Style: modal opening button

--- src/app/sub-components/buttons/ButtonsSvg.jsx
+++ src/app/sub-components/buttons/ButtonsSvg.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function ButtonsSvg({ svg, fct }) {
   return (
-    <button onClick={fct} className="w-10 h-10">
+    <button
+      onClick={fct}
+      className="w-8 h-8 animate-pulse hover:animate-none hover:bg-white rounded-full"
+    >
       {svg}
     </button>
   );
