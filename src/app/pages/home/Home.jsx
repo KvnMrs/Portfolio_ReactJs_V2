c26dc216ca90799d9ce7ad129d3308fc0529a416
Kévin Marais <kevin.mrs2020@gmail.com>
2022-08-15 09:29:45 +0200

    Adding SocialMedias to the home section

--- src/app/pages/home/Home.jsx
+++ src/app/pages/home/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+// Assets
+import Github from "../../../assets/img/imgSocialMedias/Github.png";
+import Linkedin from "../../../assets/img/imgSocialMedias/Linkedin.png";
+import SocialMediasLinks from "../../sub-components/Links/SocialMediasLinks";
+
 function Home() {
   return (
     <>
@@ -9,6 +14,18 @@ function Home() {
             Bonjour et bienvenue sur mon Portfolio. 🙂
           </h1>
           <p>Je suis Kévin Marais Développeur Web full stack.</p>
+          <div className="social-medias-onscroll">
+            <SocialMediasLinks
+              link={"https://www.linkedin.com/in/k%C3%A9vin-marais-861314216/"}
+              img={Linkedin}
+              alt={"Lien vers Linkedin"}
+            />
+            <SocialMediasLinks
+              link={"https://github.com/KvnMrs"}
+              img={Github}
+              alt={"Lien vers Github"}
+            />
+          </div>
         </header>
       </div>
     </>
