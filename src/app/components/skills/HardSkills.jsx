7c9729c1c1bb335f604362478982311ce1bc0cfe
Kévin Marais <kevin.mrs2020@gmail.com>
2022-08-25 15:31:12 +0200

    Replace current button by ButtonSvg

--- src/app/components/skills/HardSkills.jsx
+++ src/app/components/skills/HardSkills.jsx
@@ -3,29 +3,23 @@ import React from "react";
 // Datas
 import dataSkills from "../../../datas/datasSkills";
 
+// Sub-components
+import ButtonsSvg from "../../sub-components/buttons/ButtonsSvg";
+
+// Assets
+import LeftArrowSvg from "../../../assets/svg/global/LeftArrowSvg";
+
 const HardSkills = ({ toggleSkills }) => {
   return (
     <>
-      <article className="HardSkills-container">
+      <article className="hardSkills-container">
         <header>
-          <h2 className="ml-16">Compétences</h2>
-          <button
-            type="button"
-            className="group ml-5"
-            onClick={() => {
-              toggleSkills();
-            }}
-          >
-            <svg
-              className="group-hover:fill-cyan-600 w-8"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 500 500"
-              style={{ enableBackground: "new 0 0 500 500" }}
-              fill="white"
-            >
-              <path d="M329.4 248.4c-2.4-1.8-5.1-3.4-7.2-5.5C256 176.8 189.8 110.6 123.6 44.5c-4.7-4.7-9.1-9.4-10-16.4-1.4-10.2 1.9-18.4 10.5-24.2 8.1-5.5 18.9-5.1 27 .7 2.4 1.8 4.6 3.9 6.7 6 73 72.9 145.9 145.8 218.8 218.9 4 4 7.5 9 9.4 14.2 2.8 7.8.7 15.4-4.7 21.9-1.3 1.5-2.7 3-4.2 4.4-73.5 73.6-147 147-220.4 220.6-5.6 5.5-11.6 9.6-19.8 9.4-9.8-.2-17-4.6-21.2-13.3-4.2-8.6-3.4-17.2 2.3-25 1.8-2.4 4-4.5 6.2-6.6 66.2-66.2 132.3-132.4 198.5-198.5 1.9-1.9 4.4-3.4 6.6-5 0-1.1.1-2.1.1-3.2z" />
-            </svg>
-          </button>
+          <h2 className="hardSkills-title ml-16">Compétences</h2>
+          <ButtonsSvg
+            svg={<LeftArrowSvg />}
+            fct={() => toggleSkills()}
+            customClass={"group ml-5"}
+          />
         </header>
 
         <aside>
--- src/app/sub-components/buttons/ButtonsSvg.jsx
+++ src/app/sub-components/buttons/ButtonsSvg.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-function ButtonsSvg({ svg, fct }) {
+function ButtonsSvg({ svg, fct, customClass }) {
   return (
     <button
       onClick={fct}
-      className="w-8 h-8 animate-pulse hover:animate-none hover:ring-2 duration-200 ease-in-out ring-white rounded-full"
+      className={
+        "w-8 h-8 animate-pulse hover:animate-none hover:ring-2 duration-200 ease-in-out ring-white rounded-full" +
+        ` ${customClass}`
+      }
     >
       {svg}
     </button>
