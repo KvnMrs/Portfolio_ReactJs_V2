69c0939107ce527ed316be0db5413097f4c2a417
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-01 09:24:24 +0200

    About animations

--- src/app/pages/about/About.jsx
+++ src/app/pages/about/About.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+//Modules
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 // Components
 import SoftSkills from "../../components/about/SoftSkills";
@@ -16,34 +20,128 @@ import imgProfil from "../../../assets/img/imgAbout/Kevin.jpg";
 //Datas
 import dataSkills from "../../../datas/datasSkills";
 
-function About() {
-  const [activeModal, setActiveModal] = useState(false);
+//Hooks
+import { useOnScreen } from "../../hooks/useOnScreen";
 
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+function About() {
+  const [activedAnimation, setActivedAnimation] = useState(false);
+  const [activeModal, setActiveModal] = useState(false);
+  const refAbout = useRef();
+
+  const onScreen = useOnScreen(refAbout);
+  console.log("onScreen -->", onScreen);
+
+  // OpenModal
   const openModal = () => {
     setActiveModal(!activeModal);
   };
 
+  // Animations
+  const animationTextDescription = useCallback(
+    (element) => {
+      setActivedAnimation(true);
+      gsap.fromTo(
+        element,
+        {
+          opacity: 0,
+          scale: 0.1,
+          // scrollTrigger: {
+          //   trigger: element,
+          //   start: "top center",
+          //   end: "bottom center",
+          // },
+        },
+        {
+          duration: 1,
+          opacity: 1,
+          scale: 1,
+        }
+      );
+    },
+    [onScreen]
+  );
+
+  const animationImage = (element) => {
+    setActivedAnimation(true);
+    gsap.fromTo(
+      element,
+      {
+        opacity: 0,
+        scale: 0.1,
+        // scrollTrigger: {
+        //   trigger: element,
+        //   start: "top center",
+        //   end: "bottom center",
+        // },
+      },
+      {
+        delay: 1,
+        duration: 2,
+        opacity: 1,
+        scale: 1,
+      }
+    );
+  };
+
+  const animationSoftSkills = (element) => {
+    setActivedAnimation(true);
+    gsap.fromTo(
+      element,
+      {
+        opacity: 0,
+        scale: 0.1,
+        // scrollTrigger: {
+        //   trigger: element,
+        //   start: "top center",
+        //   end: "bottom center",
+        // },
+      },
+      {
+        delay: 2,
+        duration: 3,
+        opacity: 1,
+        scale: 1,
+      }
+    );
+  };
+  // End animations
+
+  useEffect(() => {
+    onScreen && !activedAnimation && animationTextDescription("#box1");
+  }, [onScreen]);
+
+  useEffect(() => {
+    onScreen && !activedAnimation && animationImage("#box2");
+  }, [onScreen]);
+
+  useEffect(() => {
+    onScreen && !activedAnimation && animationSoftSkills("#box3");
+  }, [onScreen]);
+
   return (
     <>
-      <section id="about" className="about-container">
+      <section id="about" ref={refAbout} className="about-container">
         <div className="card">
           <article className="description-part">
-            <aside className="text-part">
+            <aside className="text-part" id="box1">
               <h2 className="text-title">En quelques mots...</h2>
               <p className="text-description">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                 auctor lacus nec sem volutpat tristique. Interdum et malesuada
-                fames ac ante ipsum primis in faucibus. Aenean rutrum eros vitae
+                fames ac nte ipsum primis in faucibus. Aenean rutrum eros vitae
                 metus mollis bibendum. Aenean ultricies euismod turpis, et
-                maximusex. Mauris blandit, est accumsan molestie rutrum, nisl
+                maximsex. Mauris blandit, est accumsan molestie rutrum, nisl
                 urnamaximus ligula, nec condimentum lectus mauris eu leo. Morbi
                 euismod, magna id auctor volutpat, sem tellus pulvinar eros, vel
                 accumsan arcu felis et nibh. Nulla convallis massa in lacus
                 lacinia, in placerat elit mattis. Suspendisse et lacinia leo.
               </p>
             </aside>
-            <aside className="img-infos-part">
+            <aside className="img-infos-part" id="box2">
               <img className="prolfile-img" alt="Profil" src={imgProfil} />
+
               <ButtonCallToAction
                 text={"En savoir plus"}
                 customClass="md:mt-16"
@@ -53,7 +151,7 @@ function About() {
               </ButtonCallToAction>
             </aside>
           </article>
-          <article className="softSkills-part">
+          <article className="softSkills-part" id="box3">
             {dataSkills
               .filter((atout) => atout.catégorie === "Atout")
               .map((el) => (
