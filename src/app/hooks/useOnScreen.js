9bc93ed7b97d0ac53de2d8bdbd2aefc6c6304b29
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-01 09:18:38 +0200

    Creation Hook for know if element is onScrenn

---
+++ src/app/hooks/useOnScreen.js
@@ -0,0 +1,16 @@
+import { RefObject, useEffect, useState } from "react";
+
+export const useOnScreen = (ref, observerOptions) => {
+  const [isVisible, setVisibility] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisibility(entry.isIntersecting);
+    }, observerOptions);
+
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [observerOptions, ref]);
+  return isVisible;
+};
