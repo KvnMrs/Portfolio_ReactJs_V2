ba16f93f9b4e058a8370dec8067ea440fb6d78be
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-03 00:14:05 +0200

    Add contact

--- src/app/sub-components/modal/ProfileInformation.jsx
+++ src/app/sub-components/modal/ProfileInformation.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function ProfileInformation({ svg, text }) {
   return (
     <>
-      <div className="w-4/6 flex flex-col my-2 md:my-4">
+      <div className="w-full flex my-2 md:my-4">
         <span className="self-center m-2 bg-white bg-opacity-40 rounded-lg p-1">
           {svg}
         </span>
-        <p className="self-center text-center">{text}</p>
+        <p className="self-center">{text}</p>
       </div>
     </>
   );
