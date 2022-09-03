f5ed4b6e72d3fb88c02d5cfe7ca8459071ee0af0
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-03 20:03:26 +0200

    Add active behavior on modal tabs

--- src/app/components/modal/Modal.jsx
+++ src/app/components/modal/Modal.jsx
@@ -19,6 +19,8 @@ function Modal({
   const [activeTab, setActiveTab] = useState(1);
   const [activeContent, setActiveContent] = useState(component1);
 
+  const activeClass = "h-10 bg-cyan-800 border-b border-white ring-2";
+
   const toggleContent = (activeTab) => {
     if (activeTab === 1) return setActiveContent(component1);
     if (activeTab === 2) return setActiveContent(component2);
@@ -54,9 +56,21 @@ function Modal({
       <div className="modal-container">
         <div className="modal-sub-container" id="boxModal">
           <div className="content-tabs">
-            <ModalTabButtons tab={tab} fct={() => setActiveTab(1)} />
-            <ModalTabButtons tab={tab2} fct={() => setActiveTab(2)} />
-            <ModalTabButtons tab={tab3} fct={() => setActiveTab(3)} />
+            <ModalTabButtons
+              customClass={activeTab !== 1 ? "" : { activeClass }}
+              tab={tab}
+              fct={() => setActiveTab(1)}
+            />
+            <ModalTabButtons
+              customClass={activeTab !== 2 ? "" : { activeClass }}
+              tab={tab2}
+              fct={() => setActiveTab(2)}
+            />
+            <ModalTabButtons
+              customClass={activeTab !== 3 ? "" : { activeClass }}
+              tab={tab3}
+              fct={() => setActiveTab(3)}
+            />
             <button onClick={showModal} className="button-close">
               x
             </button>
