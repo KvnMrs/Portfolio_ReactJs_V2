4883fb09c4d8ec21f2d6520d39aa49ef30e67c85
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-04 16:31:42 +0200

    review responsive About section

--- src/app/components/skills/HardSkills.jsx
+++ src/app/components/skills/HardSkills.jsx
@@ -15,7 +15,7 @@ const HardSkills = ({ toggleSkills }) => {
     <>
       <article className="hardSkills-container">
         <header className="hardSkills-header">
-          <h2 className="hardSkills-title ml-16">Compétences</h2>
+          <h2 className="hardSkills-title ml-10 md:ml-16">Compétences</h2>
           <ButtonCallToActionSvg
             svg={<RightArrowSvg />}
             fct={() => toggleSkills()}
