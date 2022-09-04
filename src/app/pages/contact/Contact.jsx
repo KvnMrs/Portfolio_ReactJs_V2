ba0a83c23e8170e0e829f54897801a0792c5947d
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-05 00:23:13 +0200

    responsive Contact

--- src/app/pages/contact/Contact.jsx
+++ src/app/pages/contact/Contact.jsx
@@ -13,77 +13,81 @@ const Contact = () => {
 
   return (
     <>
-      <div id="contact" className="Contact-container bg-section-card">
-        <p className="Contact-text">
-          Un contact professionel, une question, un feedback <br />
-          contactez-moi 📝
-        </p>
-        <section className="form-container">
-          <form className="form">
-            <div>
-              <label htmlFor="reason">Motif :</label>
-              <select id="reason" onChange={(e) => setReason(e.target.value)}>
-                <option>Contact professionel</option>
-                <option>Question(s)</option>
-                <option>feedback</option>
-              </select>
+      <section id="contact" className="section-container">
+        <div className="section-surface">
+          <article className="section-sub-container">
+            <header className="section-header">
+              <p className="contact-text">
+                Un contact professionel, une question, un feedback <br />
+                contactez-moi 📝
+              </p>
+            </header>
+            <form className="form">
+              <div>
+                <label htmlFor="reason">Motif :</label>
+                <select id="reason" onChange={(e) => setReason(e.target.value)}>
+                  <option>Contact professionel</option>
+                  <option>Question(s)</option>
+                  <option>feedback</option>
+                </select>
 
-              <label htmlFor="nom">Prénom :</label>
-              <input
-                id="nom"
-                type="nom"
-                name="nom"
-                value={firstname}
-                onChange={(e) => setFirstname(e.target.value)}
-              />
-              <label htmlFor="prenom">Nom :</label>
-              <input
-                id="prenom"
-                type="prenom"
-                name="prenom"
-                value={lastname}
-                onChange={(e) => setLastname(e.target.value)}
-              />
-              <label htmlFor="email">Adresse Mail :</label>
-              <input
-                id="email"
-                type="email"
-                name="email"
-                value={mail}
-                onChange={(e) => setMail(e.target.value)}
-              />
-              <label htmlFor="phone">Téléphone :</label>
-              <input
-                id="phone"
-                type="phone"
-                name="phone"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="message">Votre message :</label>
-              <textarea
-                id="message"
-                name="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-              <ButtonCallToActionText
-                type={"submit"}
-                customClass={"submit-button"}
-              >
-                <a
-                  className="w-full text-sm md:text-lg"
-                  href={`mailto:kevin.mrs2020@gmail.com?subject=${reason}&body=${firstname} ${lastname.toUpperCase()}%0A${mail}%0A${phone}%0A%0A${message}`}
+                <label htmlFor="nom">Prénom :</label>
+                <input
+                  id="nom"
+                  type="nom"
+                  name="nom"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                />
+                <label htmlFor="prenom">Nom :</label>
+                <input
+                  id="prenom"
+                  type="prenom"
+                  name="prenom"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                />
+                <label htmlFor="email">Adresse Mail :</label>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  value={mail}
+                  onChange={(e) => setMail(e.target.value)}
+                />
+                <label htmlFor="phone">Téléphone :</label>
+                <input
+                  id="phone"
+                  type="phone"
+                  name="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Votre message :</label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                <ButtonCallToActionText
+                  type={"submit"}
+                  customClass={"submit-button"}
                 >
-                  Envoyer
-                </a>
-              </ButtonCallToActionText>
-            </div>
-          </form>
-        </section>
-      </div>
+                  <a
+                    className="w-full text-sm md:text-lg"
+                    href={`mailto:kevin.mrs2020@gmail.com?subject=${reason}&body=${firstname} ${lastname.toUpperCase()}%0A${mail}%0A${phone}%0A%0A${message}`}
+                  >
+                    Envoyer
+                  </a>
+                </ButtonCallToActionText>
+              </div>
+            </form>
+          </article>
+        </div>
+      </section>
     </>
   );
 };
