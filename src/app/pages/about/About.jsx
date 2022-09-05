ea63496d801c3b2e1e407c01ef03fd3c1f2a0a64
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-05 17:48:43 +0200

    use same architecture of class than other components

--- src/app/pages/about/About.jsx
+++ src/app/pages/about/About.jsx
@@ -28,43 +28,48 @@ function About() {
     <>
       <section id="about" className="section-container">
         <div className="section-surface">
-          <article className="description-part">
-            <aside className="description-text">
-              <h1 className="description-title">En quelques mots...</h1>
-              <p className="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                auctor lacus nec sem volutpat tristique. Interdum et malesuada
-                fames ac nte ipsum primis in faucibus. Aenean rutrum eros vitae
-                metus mollis bibendum. Aenean ultricies euismod turpis, et
-                maximsex. Mauris blandit, est accumsan molestie rutrum, nisl
-                urnamaximus ligula, nec condimentum lectus mauris eu leo. Morbi
-                euismod, magna id auctor volutpat, sem tellus pulvinar eros, vel
-                accumsan arcu felis et nibh. Nulla convallis massa in lacus
-                lacinia, in placerat elit mattis. Suspendisse et lacinia leo.
-              </p>
-            </aside>
-            <aside className="img-button-part">
-              <img className="profile-img" alt="Profil" src={imgProfil} />
+          <article className="section-sub-container">
+            <header className="section-header">
+              <h1 className="section-title">En quelques mots...</h1>
+            </header>
+            <div className="description-part">
+              <aside className="description-text">
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  auctor lacus nec sem volutpat tristique. Interdum et malesuada
+                  fames ac nte ipsum primis in faucibus. Aenean rutrum eros
+                  vitae metus mollis bibendum. Aenean ultricies euismod turpis,
+                  et maximsex. Mauris blandit, est accumsan molestie rutrum,
+                  nisl urnamaximus ligula, nec condimentum lectus mauris eu leo.
+                  Morbi euismod, magna id auctor volutpat, sem tellus pulvinar
+                  eros, vel accumsan arcu felis et nibh. Nulla convallis massa
+                  in lacus lacinia, in placerat elit mattis. Suspendisse et
+                  lacinia leo.
+                </p>
+              </aside>
+              <aside className="img-button-part">
+                <img className="profile-img" alt="Profil" src={imgProfil} />
 
-              <ButtonCallToActionText
-                text={"En savoir plus"}
-                customClass="m-auto"
-                fct={openModal}
-              ></ButtonCallToActionText>
-            </aside>
-          </article>
+                <ButtonCallToActionText
+                  text={"En savoir plus"}
+                  customClass="m-auto"
+                  fct={openModal}
+                ></ButtonCallToActionText>
+              </aside>
+            </div>
 
-          <article className="softSkills-part">
-            {dataSkills
-              .filter((atout) => atout.catégorie === "Atout")
-              .map((el) => (
-                <SoftSkills
-                  key={el.element}
-                  softSkill={el.element}
-                  categorie={el.catégorie}
-                  img={el.image}
-                />
-              ))}
+            <aside className="softSkills-part">
+              {dataSkills
+                .filter((atout) => atout.catégorie === "Atout")
+                .map((el) => (
+                  <SoftSkills
+                    key={el.element}
+                    softSkill={el.element}
+                    categorie={el.catégorie}
+                    img={el.image}
+                  />
+                ))}
+            </aside>
           </article>
         </div>
         {activeModal ? (
