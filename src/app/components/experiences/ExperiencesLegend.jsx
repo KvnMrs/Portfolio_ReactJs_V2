49da6485eea5118b8770842335be33fd842333c6
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-07 23:46:19 +0200

    Add style on the legend part

--- src/app/components/experiences/ExperiencesLegend.jsx
+++ src/app/components/experiences/ExperiencesLegend.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
 
-const ExperiencesLegend = () => {
-  const [activeLegend, setActiveLegend] = useState(false);
+const ExperiencesLegend = ({ legend, showLegend }) => {
   return (
     <>
-      <div className="legend-experience-container">
-        <div className="legend-experience-sub-container">
-          <div className="flex place-content-center items-center mb-5">
+      <div className="legend-container">
+        <div className="legend-sub-container">
+          <div className="legend-checkbox-container">
             <input
               type={"checkbox"}
-              name="Types expériences"
-              className="w-5 h-5 mx-2"
-              onClick={() => setActiveLegend(!activeLegend)}
+              value={legend}
+              checked={legend}
+              name="legend"
+              className="legend-checkbox"
+              onChange={showLegend}
             />
-            <label htmlFor="Types expériences">Types expériences</label>
+            <label htmlFor="legend" className="underline">
+              Types expériences
+            </label>
           </div>
-          <ul className="legend-experience-list">
-            <li className="legend-experience-list-elements">
-              <div className="legend-elements-circles bg-red-400"></div>
+          <ul className="legend-list">
+            <li className="legend-list-elements">
+              <div className="legend-elements-circles bg-purple-500"></div>
               <p className="ml-2">Developement web</p>
             </li>
-            <li className="legend-experience-list-elements">
-              <div className="legend-elements-circles bg-green-400"></div>
+            <li className="legend-list-elements">
+              <div className="legend-elements-circles bg-green-500"></div>
               <p className="ml-2">Restauration</p>
             </li>
-            <li className="legend-experience-list-elements">
-              <div className="legend-elements-circles bg-blue-400"></div>
+            <li className="legend-list-elements">
+              <div className="legend-elements-circles bg-amber-500"></div>
               <p className="ml-2">Coiffure</p>
             </li>
-            <li className="legend-experience-list-elements">
-              <div className="legend-elements-circles bg-yellow-400"></div>
+            <li className="legend-list-elements">
+              <div className="legend-elements-circles bg-blue-600"></div>
               <p className="ml-2">Autres</p>
             </li>
           </ul>
