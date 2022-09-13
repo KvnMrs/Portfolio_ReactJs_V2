468647c8e4e6db55ce27742c3727f193bc3006b7
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-13 11:34:11 +0200

    remove video on home page

--- src/app/pages/home/Home.jsx
+++ src/app/pages/home/Home.jsx
@@ -3,9 +3,6 @@ import React from "react";
 // Packages
 import { Link } from "react-router-dom";
 
-// Assets
-import videoBg from "../../../assets/video/video-bg.webm";
-
 //Sub-component
 import ButtonCallToActionText from "../../sub-components/buttons/ButtonCallToActionText";
 
@@ -13,14 +10,6 @@ function Home() {
   return (
     <>
       <div id="home" className="home-container h-screen relative">
-        <video
-          src={videoBg}
-          type="video/mp4"
-          autoPlay
-          muted
-          loop
-          className="absolute w-full h-full object-cover"
-        />
         <header className="home-header z-10">
           <h1 className="welcome">Bienvenue sur mon Portfolio</h1>
           <p>
