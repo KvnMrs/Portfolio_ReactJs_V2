1ce0041d0405b8fb8e2446b18fec4e69ae42dc70
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-13 09:29:27 +0200

    remove video

--- src/app/pages/home/Home.jsx
+++ src/app/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import Github from "../../../assets/img/imgSocialMedias/Github.png";
 import Linkedin from "../../../assets/img/imgSocialMedias/Linkedin.png";
 import ButtonCallToActionText from "../../sub-components/buttons/ButtonCallToActionText";
-import videoBg from "../../../assets/video/videoBg.mp4";
+
 //Sub-component
 import SocialMediasLinks from "../../sub-components/Links/SocialMediasLinks";
 
@@ -25,15 +25,7 @@ function Home() {
   return (
     <>
       <div id="home" className="home-container h-screen relative">
-        <video
-          src={videoBg}
-          type="video/mp4"
-          autoPlay
-          muted
-          loop
-          className="absolute w-full h-full object-cover"
-        />
-        <header className="home-header z-10">
+        <header className="bg-black p-20 bg-opacity-40 home-header z-50">
           <h1 className="welcome">Bienvenue sur mon Portfolio</h1>
           <p>
             <span className="name">Kévin Marais</span>
