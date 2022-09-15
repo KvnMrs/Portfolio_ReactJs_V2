db240da324b591109966f787d74c03e32d51f994
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-15 22:33:48 +0200

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import "./App.css";
 
-<<<<<<< HEAD
-// Modules
-import { Routes, Route } from "react-router-dom";
-
-// pages
-=======
 // Package
 import { Routes, Route } from "react-router-dom";
 import { motion } from "framer-motion";
 
 // Pages
->>>>>>> develop
 import Home from "./app/pages/home/Home";
 import MyApp from "./app/MyApp";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <div className="scroll-control">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/portofolio" element={<MyApp />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </div>
-=======
       <motion.div
         className="scroll-control"
         initial={{ opacity: 0 }}
@@ -37,7 +21,6 @@ function App() {
           <Route path="/portofolio" element={<MyApp />} />
         </Routes>
       </motion.div>
->>>>>>> develop
     </>
   );
 }
