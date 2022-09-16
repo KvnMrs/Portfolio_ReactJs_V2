1750dcf167c1a4d0c1614201e52af06010f17431
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-16 23:31:34 +0200

    create animation file for About

---
+++ src/app/animations/aboutAnimation.js
@@ -0,0 +1,33 @@
+export const descriptionAnim = {
+  initial: { opacity: 0, scale: 0.5 },
+  animate: { opacity: 1, scale: 1, transition: { duration: 0.7 } },
+};
+
+export const imageAnim = {
+  initial: { opacity: 0, scale: 0.5 },
+  animate: { opacity: 1, scale: 1, transition: { duration: 0.7, delay: 0.7 } },
+};
+
+export const softSkillsAnim = {
+  initial: { opacity: 0, scale: 0.5 },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 0.7, delay: 1.4 },
+  },
+};
+
+export const imageHoverAnim = {
+  whileHover: {
+    borderColor: "cyan",
+    scale: 1.2,
+  },
+  transition: { duration: 0 },
+};
+
+export const softSkillHoverAnim = {
+  whileHover: {
+    scale: 1.2,
+  },
+  transition: { duration: 0 },
+};
--- src/app/components/about/SoftSkills.jsx
+++ src/app/components/about/SoftSkills.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 // Package
 import { motion } from "framer-motion";
 
+// Animations
+import { softSkillHoverAnim } from "../../animations/aboutAnimation";
+
 const SoftSkills = ({ softSkill, categorie, img, openModal }) => {
   const [activeHover, setActiveHover] = useState(false);
   return (
@@ -10,22 +13,18 @@ const SoftSkills = ({ softSkill, categorie, img, openModal }) => {
       <>
         <motion.aside
           className="softSkill"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
           onHoverStart={() => {
             setActiveHover(true);
           }}
           onHoverEnd={() => {
             setActiveHover(false);
           }}
-          transition={{ duration: 0.7, delay: 1.4 }}
         >
           <motion.button
             type="button"
             onClick={openModal}
-            whileHover={{
-              scale: 1.2,
-            }}
+            variants={softSkillHoverAnim}
+            whileHover="whileHover"
           >
             <h4 className="softSkill-title">{softSkill}</h4>
 
