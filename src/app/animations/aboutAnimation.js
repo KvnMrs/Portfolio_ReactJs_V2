4a36b69b1efba14e7eca66d0f0d74378e2d6b96e
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-21 10:53:55 +0200

    review animations of contact and about

--- src/app/animations/aboutAnimation.js
+++ src/app/animations/aboutAnimation.js
@@ -1,12 +1,12 @@
 // About
 export const descriptionAnim = {
-  initial: { opacity: 0, scale: 0.5 },
-  animate: { opacity: 1, scale: 1, transition: { duration: 0.7 } },
+  initial: { opacity: 0, x: -80 },
+  animate: { opacity: 1, x: 0, transition: { duration: 0.7 } },
 };
 
 export const imageAnim = {
-  initial: { opacity: 0, scale: 0.5 },
-  animate: { opacity: 1, scale: 1, transition: { duration: 0.7, delay: 0.7 } },
+  initial: { opacity: 0, x: 80 },
+  animate: { opacity: 1, x: 0, transition: { duration: 0.7 } },
 };
 
 export const softSkillsAnim = {
@@ -14,7 +14,7 @@ export const softSkillsAnim = {
   animate: {
     opacity: 1,
     scale: 1,
-    transition: { duration: 0.7, delay: 1.4 },
+    transition: { duration: 0.7, delay: 0.7 },
   },
 };
 
