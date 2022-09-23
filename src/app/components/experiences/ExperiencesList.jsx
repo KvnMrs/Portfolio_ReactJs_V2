577d4f3d6167f32f5c774567c3c8f9403f9c85ae
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-23 17:22:52 +0200

    review style experience

--- src/app/components/experiences/ExperiencesList.jsx
+++ src/app/components/experiences/ExperiencesList.jsx
@@ -13,11 +13,28 @@ function ExperiencesList({ datas }) {
         viewport={{ once: true }}
         transition={{ duration: 0.7 }}
       >
-        <h1 className="text-2xl md:text-3xl text-white">{datas.entreprise}</h1>
-        <h2 className="text-xl text-slate-200">{datas.lieu}</h2>
-        <h2 className="text-xl text-slate-200">{datas.date}</h2>
-        <h3 className="text-lg text-slate-200">{datas.statut}</h3>
-        <p className="text-lg text-slate-200 italic">{datas.mission}</p>
+        <h1 className="mb-2 text-2xl md:text-3xl text-white">
+          {datas.entreprise}
+        </h1>
+        <h2 className="mb-1 md:text-xl text-slate-200">{datas.lieu}</h2>
+        <h2 className="mb-1 md:text-xl text-slate-200">{datas.date}</h2>
+        <h3 className="mb-5 md:text-xl text-slate-200">{datas.statut}</h3>
+        <div className="w-3/4 md:w-5/6 mx-auto text-start">
+          <p className="md:text-lg text-slate-200 italic">{datas.mission}</p>
+          {datas.mission2 && (
+            <>
+              <p className="md:text-lg text-slate-200 italic">
+                {datas.mission2}
+              </p>
+              <p className="md:text-lg text-slate-200 italic">
+                {datas.mission3}
+              </p>
+              <p className="md:text-lg text-slate-200 italic">
+                {datas.mission4}
+              </p>
+            </>
+          )}
+        </div>
       </motion.div>
     </>
   );
