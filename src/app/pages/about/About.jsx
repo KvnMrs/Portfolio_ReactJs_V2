52654ca8594f0dc5601930550f81dbc4513790ac
KvnMrs <kevin.mrs2020@gmail.com>
2024-04-01 12:24:48 +0200

    refact: move the current about page to home page.

--- src/app/pages/about/About.jsx
+++ src/app/pages/about/About.jsx
@@ -1,100 +1,29 @@
-import React, { useState } from "react";
-
-// Package
-import { AnimatePresence, motion } from "framer-motion";
-
-// Components
-import SoftSkills from "../../components/about/SoftSkills";
-import Modal from "../../components/modal/Modal";
-import ProfileModal from "../../components/about/ProfileModal";
-import SoftSkillsModal from "../../components/about/SoftSkillsModal";
-
-//Sub-components
-import SocialMediasLinks from "../../sub-components/Links/SocialMediasLinks";
-
-//Assets
-import imgProfil from "../../../assets/img/imgAbout/Kevin.jpg";
-import Github from "../../../assets/img/imgSocialMedias/Github.png";
-import Linkedin from "../../../assets/img/imgSocialMedias/Linkedin.png";
-
-//Datas
-import dataSkills from "../../../datas/datasSkills";
-
+import React from "react";
+// Packages
+import { motion } from "framer-motion";
 // Animations
-import {
-  descriptionAnim,
-  imageAnim,
-  softSkillsAnim,
-  imageHoverAnim,
-} from "../../animations/aboutAnimation";
+import { descriptionAnim } from "../../animations/aboutAnimation";
 
 function About() {
-  const [activeModal, setActiveModal] = useState(false);
-
-  // OpenModal
-  const openModal = () => {
-    setActiveModal(!activeModal);
-  };
-
   return (
     <>
       <section id="about" className="w-full h-screen">
         <article className="sections-sub-containers">
-          <div className="presentation">
+          <div className="">
             <motion.aside
-              className=""
+              className="flex flex-col gap-2"
               variants={descriptionAnim}
               initial="initial"
               animate="animate"
             >
-              <p className="text-5xl">Kévin</p>
-              <p className="text-6xl">MARAIS</p>
-              <p className="text-3xl">Recherche Alternance</p>
-              <p className="text-xl">Développeur Web Full-Stack</p>
-            </motion.aside>
-            <motion.aside
-              className="picture-scoial-medias"
-              variants={imageAnim}
-              initial="initial"
-              animate="animate"
-            >
-              <button type="button" onClick={openModal}>
-                <motion.img
-                  className="profile-img"
-                  alt="Profil"
-                  src={imgProfil}
-                  variants={imageHoverAnim}
-                  whileHover="whileHover"
-                  transition="transition"
-                />
-              </button>
-              <div className="links-social-medias">
-                <SocialMediasLinks
-                  link={"https://www.linkedin.com/in/k%C3%A9vin-marais-861314216/"}
-                  img={Linkedin}
-                  alt={"Lien vers Linkedin"}
-                />
-                <SocialMediasLinks
-                  link={"https://github.com/KvnMrs"}
-                  img={Github}
-                  alt={"Lien vers Github"}
-                />
-              </div> 
+              <p className="text-3xl">Je suis..</p>
+              <p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </motion.aside>
           </div>
         </article>
       </section>
-      <AnimatePresence>
-        {activeModal && (
-          <Modal
-            showModal={openModal}
-            component1={<ProfileModal />}
-            component2={<SoftSkillsModal />}
-            tab={"Profil"}
-            tab2={"Savoir-être"}
-          />
-        )}
-      </AnimatePresence>
     </>
   );
 }
