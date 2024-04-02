f039b0e1978a92212a27a7e7c5d65938b2627334
KvnMrs <kevin.mrs2020@gmail.com>
2024-04-02 23:51:49 +0200

    style: update About. (WIP)

--- src/app/pages/about/About.jsx
+++ src/app/pages/about/About.jsx
@@ -1,28 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // Packages
 import { motion } from "framer-motion";
 // Animations
-import { descriptionAnim } from "../../animations/aboutAnimation";
+import { descriptionAnim, softSkillsAnim } from "../../animations/aboutAnimation";
+// Components
+import SoftSkills from "../../components/about/SoftSkills";
+// Sub-components
+import ButtonCallToActionText from "../../sub-components/buttons/ButtonCallToActionText";
+// Datas
+import dataSkills from "../../../datas/datasSkills";
 
 function About() {
   return (
     <>
-      <section id="about" className="w-full h-screen">
-        <article className="sections-sub-containers">
-          <div className="">
-            <motion.aside
-              className="flex flex-col gap-2"
+      <section id="about" className="sections-containers">
+        <article className="articles-containers">
+          <aside className="about-aside-containers">
+            <div className="w-full row-span-1 content-end">
+              <div className="h-3/5 w-3/4 mt-auto ml-auto rotate-6 rounded-3xl bg-white"></div>
+            </div>
+            <motion.div
+              className="row-span-2"
               variants={descriptionAnim}
               initial="initial"
               animate="animate"
             >
-              <p className="text-3xl">Je suis..</p>
+              <h2 className="text-3xl">Titre 1.</h2>
               <p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </motion.aside>
-          </div>
+            </motion.div>
+            <motion.div
+              className="w-full flex flex-wrap self-center justify-between"
+              variants={softSkillsAnim}
+              initial="initial"
+              animate="animate"
+            >
+              {dataSkills
+                .filter((atout) => atout.catégorie === "Atout")
+                .map((el) => (
+                  <SoftSkills
+                    key={el.element}
+                    softSkill={el.element}
+                    categorie={el.catégorie}
+                    img={el.image}
+                  />
+                ))}
+            </motion.div>
+          </aside>
         </article>
+        <Link to="/portofolio" >
+          <ButtonCallToActionText
+            text={"Mes projets ?"}
+          ></ButtonCallToActionText>
+        </Link>
       </section>
     </>
   );
--- src/app/sub-components/buttons/ButtonCallToActionText.jsx
+++ src/app/sub-components/buttons/ButtonCallToActionText.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const ButtonCallToActionText = ({ children, text, fct, customClass, type }) => {
   return (
     <>
+      <div className="absolute right-4 bottom-4">
       <button
         type={type ? type : "button"}
         onClick={fct ? fct : null}
@@ -15,6 +16,7 @@ const ButtonCallToActionText = ({ children, text, fct, customClass, type }) => {
         {children ? children : null}
         <p className="text-base">{text}</p>
       </button>
+      </div>
     </>
   );
 };
