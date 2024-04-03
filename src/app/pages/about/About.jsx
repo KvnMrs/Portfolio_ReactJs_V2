3eaa7c37749df88132ab9dd18c3419a49b5a423e
KvnMrs <kevin.mrs2020@gmail.com>
2024-04-03 14:00:26 +0200

    style: update about.

--- src/app/pages/about/About.jsx
+++ src/app/pages/about/About.jsx
@@ -14,10 +14,10 @@ import dataSkills from "../../../datas/datasSkills";
 function About() {
   return (
     <>
-      <section id="about" className="sections-containers">
-        <article className="articles-containers">
+      <section id="about" className="section-containers">
+        <article className="article-containers">
           <aside className="about-aside-containers">
-            <div className="about-img-container">
+            <div className="about-img-containers self-end rotate-12">
               <div className="about-img"></div>
             </div>
             <motion.div
@@ -49,8 +49,44 @@ function About() {
                 ))}
             </motion.div>
           </aside>
+
+          <div className="w-4/5 mx-auto my-10 border border-t-0"> </div>
+
+          <aside className="about-aside-containers">
+            <div className="about-img-containers self-start rotate-[168deg]">
+              <div className="about-img"></div>
+            </div>
+            <motion.div
+              className="about-description-container text-end"
+              variants={descriptionAnim}
+              initial="initial"
+              animate="animate"
+            >
+              <h2 className="text-3xl">Titre 1.</h2>
+              <p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+            <motion.div
+              className="flex gap-4 flex-wrap justify-around"
+              variants={softSkillsAnim}
+              initial="initial"
+              animate="animate"
+            >
+              {dataSkills
+                .filter((atout) => atout.catégorie === "Compétence")
+                .map((el) => (
+                  <SoftSkills
+                    key={el.element}
+                    softSkill={el.element}
+                    categorie={el.catégorie}
+                    img={el.image}
+                  />
+                ))}
+            </motion.div>
+          </aside>
         </article>
-        <Link to="/portofolio" >
+        <Link className="flex justify-end" to="/portofolio" >
           <ButtonCallToActionText
             text={"Mes projets ?"}
           ></ButtonCallToActionText>
