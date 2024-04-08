9e1866401f214c602127c98da4b3373c3e16c0ed
KvnMrs <kevin.mrs2020@gmail.com>
2024-04-08 14:52:06 +0200

    style: Home responsive desktop.

--- src/app/pages/home/Home.jsx
+++ src/app/pages/home/Home.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// Packages
-import { AnimatePresence, motion } from "framer-motion";
-// Components
-import Modal from "../../components/modal/Modal";
-import ProfileModal from "../../components/about/ProfileModal";
-import SoftSkillsModal from "../../components/about/SoftSkillsModal";
 //Sub-components
 import SocialMediasLinks from "../../sub-components/Links/SocialMediasLinks";
 import ButtonCallToActionText from "../../sub-components/buttons/ButtonCallToActionText";
@@ -32,52 +26,44 @@ function About() {
     <>
       <section id="home" className="section-containers">
         <article className="article-containers">
-          <aside className="presentation ">
-            <motion.div
-              className="w-fit relative"
-              variants={descriptionAnim}
-              initial="initial"
-              animate="animate"
-            >
-              <p className="text-5xl">Kévin</p>
-              <p className="text-6xl">MARAIS</p>
-              <p className="text-3xl">Recherche Alternance</p>
-              <p className="text-xl">Développeur Web Full-Stack</p>
-              <motion.img
-                className="profile-img"
-                alt="Profil"
-                src={imgProfil}
-                variants={imageHoverAnim}
-                whileHover="whileHover"
-                transition="transition"
-              />
-            </motion.div>
-            <div className="links-social-medias items-center">
+          <aside className="presentation-container">
+            <div className="md:flex">
+              <div className="flex flex-col gap-2 justify-center  md:gap-4">
+                <span>
+                  <p className="text-5xl">Kévin</p>
+                  <p className="text-6xl">MARAIS</p>
+                </span>
+                <span>
+                  <p className="text-3xl">Recherche Alternance</p>
+                  <p className="text-xl">Développeur Web Full-Stack</p>
+                </span>
+              </div>
+              <img className="profile-img" alt="Profil" src={imgProfil} />
+            </div>
+
+            <div className="flex gap-8 items-center">
               <a href={CV} download>
-                <button className="mr-4 px-4 py-2 outline outline-1 rounded-md">
-                  Télécharger CV
-                </button>
+                <ButtonCallToActionText
+                  text={"Télécharger CV"}
+                ></ButtonCallToActionText>
               </a>
-              <SocialMediasLinks
-                link={
-                  "https://www.linkedin.com/in/k%C3%A9vin-marais-861314216/"
-                }
-                img={Linkedin}
-                alt={"Lien vers Linkedin"}
-              />
-              <SocialMediasLinks
-                link={"https://github.com/KvnMrs"}
-                img={Github}
-                alt={"Lien vers Github"}
-              />
+              <div className="flex gap-2">
+                <SocialMediasLinks
+                  link={
+                    "https://www.linkedin.com/in/k%C3%A9vin-marais-861314216/"
+                  }
+                  img={Linkedin}
+                  alt={"Lien vers Linkedin"}
+                />
+                <SocialMediasLinks
+                  link={"https://github.com/KvnMrs"}
+                  img={Github}
+                  alt={"Lien vers Github"}
+                />
+              </div>
             </div>
           </aside>
         </article>
-        {/* <Link className="flex justify-end" to="/about">
-          <ButtonCallToActionText
-            text={"Qui suis-je ?"}
-          ></ButtonCallToActionText>
-        </Link> */}
       </section>
     </>
   );
