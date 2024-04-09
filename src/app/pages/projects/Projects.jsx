1a49cfe8899b3f78ecbecf9e0a8dee96437c30da
KvnMrs <kevin.mrs2020@gmail.com>
2024-04-09 11:59:13 +0200

    style: responsive project desktop.

--- src/app/pages/projects/Projects.jsx
+++ src/app/pages/projects/Projects.jsx
@@ -13,34 +13,29 @@ function Projects() {
   return (
     <>
       <section id="projects" className="section-containers">
-          <article className="article-containers relative">
-            <header className="section-headers">
-              <h2 className="section-titles">Projets</h2>
-            </header>
+        <article className="article-containers relative">
+          <header className="section-headers">
+            <h2 className="section-titles">Projets</h2>
+          </header>
           <div className="project-containers">
-              {DatasProjects.map((el) => (
-                <>
-                  {
-                    <ProjectCard
-                      key={el.id}
-                      img={el.img}
-                      title={el.title}
-                      technologies={el.technologies}
-                      description={el.description}
-                      link={el.link}
-                      note={el.note}
-                      projectRefprop={itemsRef}
-                    />
-                  }
-                </>
-              ))}
+            {DatasProjects.map((el) => (
+              <>
+                {
+                  <ProjectCard
+                    key={el.id}
+                    img={el.img}
+                    title={el.title}
+                    technologies={el.technologies}
+                    description={el.description}
+                    link={el.link}
+                    note={el.note}
+                    projectRefprop={itemsRef}
+                  />
+                }
+              </>
+            ))}
           </div>
         </article>
-        <Link className="flex justify-end" to="/projects" >
-          <ButtonCallToActionText
-            text={"Me contacter"}
-          ></ButtonCallToActionText>
-        </Link>
       </section>
     </>
   );
