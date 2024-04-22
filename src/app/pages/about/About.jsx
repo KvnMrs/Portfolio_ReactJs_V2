9fc8eba105c68f0dac01154557b9d4c7e5144323
KvnMrs <kevin.mrs2020@gmail.com>
2024-04-22 15:56:49 +0200

    refact: correction's writting.

--- src/app/pages/about/About.jsx
+++ src/app/pages/about/About.jsx
@@ -14,34 +14,36 @@ function About() {
     <>
       <section id="about" className="section-containers">
         <article className="article-containers">
-          <aside className="about-aside-containers | md:self-center">
-            <img
-              className="w-40 h-40 object-cover self-center | md:w-48 md:h-48"
-              alt="Profil"
-              src={imgProfil}
-            />
-            <div className="px-2 flex flex-col gap-3 | md:w-8/12 md:px-4">
-              <p className="text-xl">
-                Je m'appelle Kévin, j'ai 30 ans et j'habite la ville culturelle
-                et historique de Nantes. C'est peut être de la que vient mon
-                intêret pour l'histoire et ces différentes époques, qui par
-                moment nous amêne à l'interogation, parfois à la compréhention
-                de certaines choses.
-              </p>
-              <p className="text-xl">
-                De nature curieux j'aime aussi beaucoup voyager dans le but de
-                découvrir et m'adapter à de nouveaux environnement. Il est
-                possible aussi que ma variable 'lookingForChallenge' soit une
-                constante initialiser à 'true'..
-              </p>
-              <p className="text-xl">
-                Vous l'avez peut-être compris, cette même curiosité m'a amenée
-                un jour à découvrir le domaine du développement web. C'est en
-                autonomie et par le biais de différents support que j'ai
-                commencé à m'initer à ces bases.
-              </p>
+          <aside className="about-aside-containers | md:self-center lg:gap-16">
+            <div className="flex flex-col gap-4 items-center justify-center | md:gap-8 | lg:flex-row-reverse ">
+              <img
+                className="w-40 h-40 object-cover self-center | lg:w-80 lg:h-[20.5rem]"
+                alt="Profil"
+                src={imgProfil}
+              />
+              <div className="px-2 flex flex-col gap-3 | lg:w-1/2 lg:px-4">
+                <p className="text-xl">
+                  Je m'appelle Kévin, j'ai 30 ans et j'habite la ville
+                  culturelle et historique de Nantes. C'est peut être de la que
+                  vient mon intêret pour l'histoire et ces différentes époques,
+                  qui par moment nous amêne à l'interogation, parfois à la
+                  compréhention de certaines choses.
+                </p>
+                <p className="text-xl">
+                  De nature curieuse, j'aime aussi beaucoup voyager dans le but
+                  de découvrir et de m'adapter à de nouveaux environnements. Il
+                  est également possible que ma variable 'lookingForChallenge'
+                  soit une constante initialisée à 'true'.
+                </p>
+                <p className="text-xl">
+                  Vous l'avez peut-être compris, cette même curiosité m'a amené
+                  un jour à découvrir le domaine du développement web. C'est en
+                  autonomie et par le biais de différents supports que j'ai
+                  commencé à m'initier à ces bases.
+                </p>
+              </div>
             </div>
-            <div className="w-11/12 flex gap-4 md:gap-8 flex-wrap justify-between | md:w-2/3 ">
+            <div className="w-11/12 flex gap-4 md:gap-8 flex-wrap justify-around | md:w-2/3 ">
               {dataSkills
                 .filter((atout) => atout.part === 1)
                 .map((el) => (
@@ -67,33 +69,32 @@ function About() {
                   {"<"} Une Aventure <br></br>avec un grand "A" {"/>"}
                 </h2>
                 <div className="flex flex-col gap-3 | md:flex-row md:gap-6">
-                  <p className="text-xl md:w-1/3">
+                  <p className="text-xl | md:w-1/3">
                     C'est après dix années passées dans le domaine du commerce
                     et suite à mon engouement pour le développement web que j'ai
                     choisi de me réorienter dans ce domaine, encore nouveau pour
-                    moi il y'a quelques années..
+                    moi il y a quelques années.
                   </p>
-                  <p className="text-xl md:w-1/3 md:mr-2">
-                    Si dans un premier temps mes sept année passé en tant que
-                    coiffeur/barbier semble loin du développement web, je me
-                    suis très vite rendu compte que mon expérience dans le
-                    suivit clientèl (relationnel, diagnostic, conseils,
-                    adaptation, réalisation) pourrait m'être utile. Soucieux de
-                    celui-ci par le passé, j'apporte aujourd'hui un réel intérêt
-                    à l'UI/UX.
+                  <p className="text-xl | md:w-1/3 md:mr-2">
+                    Si dans un premier temps mes sept années passées en tant que
+                    coiffeur/barbier semblent loin du développement web, je me
+                    suis très vite rendu compte que mon expérience dans le suivi
+                    clientèle (relationnel, diagnostic, conseils, adaptation,
+                    réalisation) pourrait m'être utile. Soucieux de celui-ci par
+                    le passé, j'apporte aujourd'hui un réel intérêt à l'UI/UX.
                   </p>
-                  <p className="text-xl md:w-1/3">
+                  <p className="text-xl | md:w-1/3">
                     Ma réorientation actuelle n'est pas ma première. Je me suis
                     aussi épanoui en tant que serveur en restauration. Un
                     domaine demandant de la rigueur, une bonne estimation des
-                    timings et respect de ces derniers pour le bon déroulement
-                    des services. Mais aussi un esprit d'esprit d'équipe et une
-                    analyse rapide des priorité par instants.
+                    timings et le respect de ces derniers pour le bon
+                    déroulement des services. Mais aussi un esprit d'équipe et
+                    une analyse rapide des priorités par instants.
                   </p>
                 </div>
               </div>
             </div>
-            <div className="flex gap-4 md:gap-8 flex-wrap justify-between | md:w-2/3 ">
+            <div className="flex gap-4 flex-wrap justify-between | md:w-2/3 md:gap-8 ">
               {dataSkills
                 .filter((atout) => atout.part === 2)
                 .map((el) => (
@@ -123,7 +124,7 @@ function About() {
                 <div className="flex flex-col gap-16 | md:flex-row md:gap-6">
                   <p className="text-xl md:w-1/3 relative">
                     Tout comme le web, les compétences se développent avec le
-                    temps. Depuis mes débuts en autodiacte, j'ai auto-financé ma
+                    temps. Depuis mes débuts en autodidacte, j'ai autofinancé ma
                     formation au sein de la Wild Code School. Une formation
                     Full-Stack axée sur l'environnement JavaScript et MySQL,
                     d'une durée de 5 mois.
@@ -133,7 +134,7 @@ function About() {
                     />
                   </p>
                   <p className="text-xl md:w-1/3 relative">
-                    A la suite de cette formation j'ai eu l'opportunité
+                    À la suite de cette formation, j'ai eu l'opportunité
                     d'intégrer l'équipe de Cascade8 pour un stage d'une durée de
                     5 mois également. Toujours dans un environnement JavaScript,
                     c'est durant ce stage que j'ai eu le plaisir de découvrir le
@@ -145,9 +146,9 @@ function About() {
                   </p>
                   <p className="text-xl md:w-1/3 relative">
                     J'ai également eu l'occasion d'accomplir un second stage au
-                    sein de XRATOR, d'une duré de 4 mois. Toujours dans un
-                    environnement Javascript, avec cette foic-ci Prisma pour la
-                    base de donnée.
+                    sein de XRATOR, d'une durée de 4 mois. Toujours dans un
+                    environnement JavaScript, avec cette fois-ci Prisma pour la
+                    base de données.
                     <img
                       src={img_VueJs}
                       className="max-h-full md:-bottom-28 -bottom-14 inset-x-0 m-auto md:flex absolute bg-cover opacity-20"
