f31666d96b27512dc13e5ad23735021763091a34
KvnMrs <kevin.mrs2020@gmail.com>
2024-05-03 16:36:11 +0200

    sstyle: project page v2. (mobile)

--- src/app/components/projects/ProjectCard.jsx
+++ src/app/components/projects/ProjectCard.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const ProjectCard = ({ projectData }) => {
   if (!projectData) return null;
   return (
-    <article
-      id={projectData.id}
-      className="project place-items-center items-center justify-center content-center | md:pt-16 "
-    >
+    <article id={projectData.id} className="project">
       <div className="flex flex-col gap-4">
-        <h3 className="project-title">{projectData.title}</h3>
+        <div className="w-full flex justify-between">
+          <h3 className="project-title">{projectData.title}</h3>
+          <span className="flex border w-8 h-8 items-end p-1 justify-center rounded-full text-xl">
+            {projectData.id}
+          </span>
+        </div>
         <img
           className="project-img "
           alt={projectData.title}
@@ -17,11 +19,25 @@ const ProjectCard = ({ projectData }) => {
       </div>
 
       <div className="flex flex-col">
-        <aside className="project-content">
-          <p className="project-description">{projectData.description}</p>
-          <p className="project-description">{projectData.note}</p>
-        </aside>
+        <p className="project-description">{projectData.description}</p>
+        <p className="project-description">{projectData.note}</p>
         <aside className="project-infos">
+          <a className="project-link" href={projectData.link} blank="">
+            Visiter
+            <svg
+              className="projects-svg"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
           <ul className="project-technos">
             {projectData.technologies.map((techno) => (
               <li>
--- src/app/pages/projects/Projects.jsx
+++ src/app/pages/projects/Projects.jsx
@@ -1,52 +1,19 @@
-import React, { useState } from "react";
-import { AnimatePresence, motion, useScroll } from "framer-motion";
-import DatasProjects from "../../../datas/datasProjects";
+import React from "react";
+import { AnimatePresence } from "framer-motion";
 // Components
 import ProjectCard from "../../components/projects/ProjectCard";
-export default function App() {
-  const [selectedProject, setSelectedProject] = useState(1);
-  const { scrollXProgress } = useScroll();
+import dataProjects from "../../../datas/datasProjects";
 
+export default function App() {
   return (
-    <div className="flex flex-col md:flex-row pt-8 justify-around  content-center">
-      <motion.div className="overflow-y-hidden overflow-y-auto md:scrollbar-hide flex gap-4 justify-around items-center self-auto | md:w-2/6  md:h-[40rem] md:flex-col md:gap-8 md:self-start md:overflow-x-hidden md:overflow-y-auto ">
-        {DatasProjects.map((el, index) => (
-          <motion.div
-            key={index}
-            className={"w-auto md:w-full flex flex-col cursor-pointer pl-4 "}
-            animate={{ borderLeft: "0.5px solid white" }}
-            whileHover={{ x: 20, borderLeft: 0 }}
-            onHoverStart={() => setSelectedProject(el.id)}
-            onClick={() => setSelectedProject(el.id)}
-          >
-            <h1 className="text-xl  w-max">{el.title}</h1>
-            <img
-              className=" h-20 md:h-auto md:w-3/4 rounded-lg"
-              alt={el.title}
-              src={el.img}
-            />
-          </motion.div>
-        ))}
-      </motion.div>
-      <div className="w-4/5 mx-auto my-8 border border-t-0 md:hidden"> </div>
+    <div className="flex flex-col items-center">
       <AnimatePresence exitBeforeEnter>
-        <div className="w-full md:w-1/2 content-center">
-          {selectedProject && (
-            <motion.div
-              key={selectedProject}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{}}
-            >
-              <ProjectCard
-                projectData={DatasProjects.find(
-                  (el) => el.id === selectedProject
-                )}
-              />
-            </motion.div>
-          )}
-        </div>
+        {dataProjects.map((el) => (
+          <>
+            <ProjectCard projectData={el} />
+            <div className="w-4/5 mx-auto my-8 border border-t-0"> </div>
+          </>
+        ))}
       </AnimatePresence>
     </div>
   );
