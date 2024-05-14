c3f7a18fe124b7c5e79b822fe46e7661febf78d2
KvnMrs <kevin.mrs2020@gmail.com>
2024-05-14 18:00:05 +0200

    fix: can send correctly email.

--- src/app/pages/contact/Contact.jsx
+++ src/app/pages/contact/Contact.jsx
@@ -1,11 +1,8 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
 // Animations
-import {
-  inputsAnim,
-  texteAreaAnim,
-  submitAnim,
-} from "../../animations/contactAnimation";
-// COomponents
+import { texteAreaAnim } from "../../animations/contactAnimation";
+// Components
 import ButtonCallToActionText from "../../components/common/button/ButtonCallToActionText";
 import SideBar from "../../../app/components/common/navigation/SideBar";
 // Libraries
@@ -13,22 +10,29 @@ import emailjs from "emailjs-com";
 
 const Contact = () => {
   const formRef = useRef();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      reason: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+    },
+  });
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_an3elxs",
-        "template_od75s98",
-        formRef.current,
-        "6mH0PNM35bHQtU6lg"
-      )
-      .then((result) => {
-        console.log("E-mail sent successfully!", result.text);
-      })
-      .catch((error) => {
-        console.error("Error sending e-mail:", error);
-      });
+  const sendEmail = async () => {
+    await emailjs.sendForm(
+      "service_an3elxs",
+      "template_od75s98",
+      formRef.current,
+      "6mH0PNM35bHQtU6lg"
+    );
+    reset();
   };
 
   return (
@@ -36,26 +40,77 @@ const Contact = () => {
       <SideBar />
       <section id="contact" className="section-containers">
         <article className="article-containers mt-8">
-          <form className="form " ref={formRef} onSubmit={sendEmail}>
+          <form
+            className="form"
+            ref={formRef}
+            onSubmit={handleSubmit(() => {
+              sendEmail();
+            })}
+          >
             <header className=" mx-auto z-30 flex justify-center "></header>
             <div className="md:px-8 md:py-8">
               <label htmlFor="reason">Motif :</label>
-              <select id="reason" name="reason">
+              <select
+                id="reason"
+                name="reason"
+                {...register("reason", { required: true })}
+              >
                 <option>Contact professionel</option>
                 <option>Question(s)</option>
                 <option>Feedback</option>
               </select>
+              {errors.reason && (
+                <p className="mb-4 -mt-4 text-red-500">
+                  Le prénom est obligatoire.
+                </p>
+              )}
 
               <label htmlFor="firstname">Prénom :</label>
-              <input id="firstname" type="firstname" name="firstname" />
+              <input
+                id="firstname"
+                name="firstname"
+                {...register("firstname", { required: true })}
+              />
+              {errors.firstname && (
+                <p className="mb-4 -mt-4 text-red-500">
+                  Le prénom est obligatoire.
+                </p>
+              )}
+
               <label htmlFor="lastname">Nom :</label>
-              <input id="lastname" type="lastname" name="lastname" />
+              <input
+                id="lastname"
+                type="lastname"
+                name="lastname"
+                {...register("lastname", { required: true })}
+              />
+              {errors.lastname && (
+                <p className="mb-4 -mt-4 text-red-500">
+                  Le nom est obligatoire.
+                </p>
+              )}
               <label htmlFor="email">Adresse Mail :</label>
-              <input id="email" type="email" name="email" />
+              <input
+                id="email"
+                type="email"
+                name="email"
+                {...register("email", { required: true })}
+              />
+              {errors.email && (
+                <p className="mb-4 -mt-4 text-red-500">L'email obligatoire.</p>
+              )}
             </div>
             <div variants={texteAreaAnim} initial="initial">
               <label htmlFor="message">Votre message :</label>
-              <textarea id="message" type="text" name="message" />
+              <textarea
+                id="message"
+                type="text"
+                name="message"
+                {...register("message", { required: true })}
+              />
+              {errors.message && (
+                <p className="mb-4 text-red-500">Un message est obligatoire.</p>
+              )}
               <ButtonCallToActionText
                 type={"submit"}
                 customClass={"submit-button"}
