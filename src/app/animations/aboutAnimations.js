a2892649f3105852fe8809e77cb0da5703626c0a
KvnMrs <kevin.mrs2020@gmail.com>
2024-06-01 15:22:21 +0200

    fix: warning messages.

--- src/app/animations/aboutAnimations.js
+++ src/app/animations/aboutAnimations.js
@@ -1,5 +1,3 @@
-import { delay } from "framer-motion";
-
 export const aboutMeDescriptionAnim = {
   initial: { opacity: 0, x: -80 },
   animate: { opacity: 1, x: 0, transition: { duration: 0.8 } },
